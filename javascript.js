54ecf04c994eb3e3786de75572528d7abc7619c7
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-28 00:17:10 +0000

    Make the js transitions a little more refined

--- javascript.js
+++ javascript.js
@@ -1,28 +1,35 @@
-const flex_panel_container = document.querySelector('#flex_panels')
+const flex_panel_container = document.querySelector('#flex_panels');
 const every_flex_panel = document.querySelectorAll('.flex_panel');
-const profile_column = document.querySelector('.profile')
+const profile_column = document.querySelector('.profile');
 
 function toggleSize() {
   this.classList.toggle('open');
-  console.log('1');
+  this.classList.toggle('open_active');
+
+  const this_id = this.id;
+
+  every_flex_panel.forEach(function(panel) {
+    if(panel.id !== this_id) {
+      panel.classList.remove('open');
+      panel.classList.remove('open_active');
+    };
+  });
 };
 
-function toggleOpenActive(e) {
-  if(e.propertyName.includes('flex')) {
-    this.classList.toggle('open_active');
-  };
-  console.log('2');
-};
+// function toggleOpenActive(e) {
+//   if(e.propertyName.includes('flex')) {
+//     this.classList.toggle('open_active');
+//   };
+// };
 
 function toggleWidth() {
   this.classList.toggle('wide');
   flex_panel_container.classList.toggle('hide');
   profile_column.classList.toggle('wide_active');
-  console.log('3');
 };
 
 every_flex_panel.forEach(panel => panel.addEventListener('click', toggleSize));
-every_flex_panel.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
+// every_flex_panel.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
 
 profile_column.addEventListener('click', toggleWidth);
 
