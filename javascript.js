a1f95404d05837ff3916e4b0901b664390fed21d
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-28 18:02:35 +0000

    Update the js so that the panels don't resize when an external link is clicked

--- javascript.js
+++ javascript.js
@@ -1,35 +1,33 @@
 const flex_panel_container = document.querySelector('#flex_panels');
 const every_flex_panel = document.querySelectorAll('.flex_panel');
 const profile_column = document.querySelector('.profile');
+// const link_tags = document.querySelectorAll('a');
 
-function toggleSize() {
-  this.classList.toggle('open');
-  this.classList.toggle('open_active');
+function toggleSize(e) {
+  if(e.target.parentElement.target !== '_blank') {
+    this.classList.toggle('open');
+    this.classList.toggle('open_active');
 
-  const this_id = this.id;
+    const this_id = this.id;
 
-  every_flex_panel.forEach(function(panel) {
-    if(panel.id !== this_id) {
-      panel.classList.remove('open');
-      panel.classList.remove('open_active');
-    };
-  });
+    every_flex_panel.forEach(function(panel) {
+      if(panel.id !== this_id) {
+        panel.classList.remove('open');
+        panel.classList.remove('open_active');
+      };
+    });
+  };
 };
 
-// function toggleOpenActive(e) {
-//   if(e.propertyName.includes('flex')) {
-//     this.classList.toggle('open_active');
-//   };
-// };
-
-function toggleWidth() {
-  this.classList.toggle('wide');
-  flex_panel_container.classList.toggle('hide');
-  profile_column.classList.toggle('wide_active');
+function toggleWidth(e) {
+  if(e.target.parentElement.target !== '_blank') {
+    this.classList.toggle('wide');
+    flex_panel_container.classList.toggle('hide');
+    profile_column.classList.toggle('wide_active');
+  };
 };
 
 every_flex_panel.forEach(panel => panel.addEventListener('click', toggleSize));
-// every_flex_panel.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
 
 profile_column.addEventListener('click', toggleWidth);
 
